7aa891800f30ef27313cea6d182b3a0e517e249a
andrew webber (personal) <andrewvwebber@googlemail.com>
2015-08-26 16:33:02 +0200

    added sudo to build scripts

--- cbfs-client/Docker/build.sh
+++ cbfs-client/Docker/build.sh
@@ -4,4 +4,4 @@ go get github.com/couchbaselabs/cbfs/tools/cbfsclient
 CGO_ENABLED=0 GOOS=linux go build -o /tmp/cbfs-client/cbfs-client.linux -a -tags netgo -ldflags '-w' github.com/couchbaselabs/cbfs/tools/cbfsclient
 cp src/github.com/andrewwebber/couchbase-array/cbfs-client/Docker/** /tmp/cbfs-client
 cp -r src/github.com/couchbaselabs/cbfs/monitor /tmp/cbfs-client/monitor
-docker build -t andrewwebber/cbfs-client /tmp/cbfs-client
+sudo docker build -t andrewwebber/cbfs-client /tmp/cbfs-client
--- cbfs/Docker/build.sh
+++ cbfs/Docker/build.sh
@@ -3,4 +3,4 @@ mkdir -p /tmp/cbfs
 go get github.com/couchbaselabs/cbfs
 CGO_ENABLED=0 GOOS=linux go build -o /tmp/cbfs/cbfs.linux -a -tags netgo -ldflags '-w' github.com/couchbaselabs/cbfs
 cp src/github.com/andrewwebber/couchbase-array/cbfs/Docker/** /tmp/cbfs
-docker build -t andrewwebber/cbfs /tmp/cbfs
+sudo docker build -t andrewwebber/cbfs /tmp/cbfs
