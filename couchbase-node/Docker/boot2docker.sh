67a3622aea768c5cf89b7ee805b1b1be9f0920f4
andrew webber (personal) <andrewvwebber@googlemail.com>
2016-02-04 16:57:14 +0100

    initialize cluster to 1GB

--- couchbase-node/Docker/boot2docker.sh
+++ couchbase-node/Docker/boot2docker.sh
@@ -1,4 +1,4 @@
 ./src/github.com/andrewwebber/couchbase-array/couchbase-node/Docker/build.sh
-docker run -d --name couchbase3 -e ETCDCTL_PEERS=http://192.168.1.5:4001 andrewwebber/couchbase-array
-docker run -d --name couchbase2 -e ETCDCTL_PEERS=http://192.168.1.5:4001 andrewwebber/couchbase-array
-docker run -d --name couchbase1 -p 8091:8091 -e ETCDCTL_PEERS=http://192.168.1.5:4001 andrewwebber/couchbase-array
+docker run -d --name couchbase3 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
+docker run -d --name couchbase2 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
+docker run -d --name couchbase1 -p 8091:8091 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
