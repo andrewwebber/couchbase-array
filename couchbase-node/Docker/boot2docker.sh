9de66d7408ba965cac4749af73dfabdce07d9661
andrew webber (personal) <andrewvwebber@googlemail.com>
2017-12-20 21:37:56 +0000

    couchbase 5.0.1

--- couchbase-node/Docker/boot2docker.sh
+++ couchbase-node/Docker/boot2docker.sh
@@ -1,4 +1,5 @@
 ./src/github.com/andrewwebber/couchbase-array/couchbase-node/Docker/build.sh
-docker run -d --name couchbase3 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
-docker run -d --name couchbase2 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
-docker run -d --name couchbase1 -p 8091:8091 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
+# docker run -d --name couchbase3 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
+# docker run -d --name couchbase2 -e ETCDCTL_PEERS=http://172.17.0.1:4001 andrewwebber/couchbase-array
+etcd &
+docker run --rm -it --name couchbase1 --net=host -e ETCDCTL_PEERS=http://localhost:4001 andrewwebber/couchbase-array:5.0.1
--- couchbase-node/Docker/build.sh
+++ couchbase-node/Docker/build.sh
@@ -3,4 +3,4 @@ mkdir -p /tmp/couchbase-array
 
 CGO_ENABLED=0 GOOS=linux go build -o /tmp/couchbase-array/couchbase-node-announce.linux -a -tags netgo -ldflags '-w' github.com/andrewwebber/couchbase-array/couchbase-node-announce
 cp src/github.com/andrewwebber/couchbase-array/couchbase-node/Docker/** /tmp/couchbase-array
-docker build -t andrewwebber/couchbase-array:enterprise /tmp/couchbase-array
+docker build -t andrewwebber/couchbase-array:5.0.1 /tmp/couchbase-array
